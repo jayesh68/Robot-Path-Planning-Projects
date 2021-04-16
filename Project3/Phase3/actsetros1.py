ed28aa3bb866d359c3ab170a088ea73e0711129b
jayesh68 <36202179+jayesh68@users.noreply.github.com>
2021-04-16 19:01:55 -0400

    Update actsetros1.py

--- Project3/Phase3/actsetros1.py
+++ Project3/Phase3/actsetros1.py
@@ -9,7 +9,7 @@ Created on Fri Apr  9 19:51:43 2021
 
 import numpy as np
 import math 
-
+import matplotlib.pyplot as plt
 class actionSet:
     
     #Function to traverse in the downward direction 
@@ -18,28 +18,37 @@ class actionSet:
         x = curr_node[0]
         y = curr_node[1]
         ang=curr_node[2]
+        #print('before act',x,y,ang)
         t = 0
         r = 3.8
         l = 35.4
+        #print('rpms',ul,ur)
         dt=0.1
         cost=0
         while t < 1:
             t = t + dt
-                       
-            dx = r * (ul + ur) * math.cos(ang*math.pi/180) * dt
-            dy = r * (ul + ur) * math.sin(ang*math.pi/180) * dt
+            dx = 0.5*r * (ul + ur) * math.cos(ang*math.pi/180) * dt
+            dy = 0.5*r * (ul + ur) * math.sin(ang*math.pi/180) * dt
             dtheta = (r / l) * (ur - ul) * dt
-            cost=cost+ math.sqrt(math.pow((0.5*r * (ul + ur) * math.cos(dtheta*math.pi/180) * dt),2)+math.pow((0.5*r * (ul + ur) * math.sin(dtheta*math.pi/180) * dt),2))
+            #print('dt',dtheta)
+            dtheta=dtheta*180/math.pi
+            ang+= dtheta
+            #print('dx','dy',dx,dy)
+            #print('ang',ang)
+            cost=cost+ math.sqrt(math.pow((0.5*r * (ul + ur) * math.cos(ang*math.pi/180) * dt),2)+math.pow((0.5*r * (ul + ur) * math.sin(ang*math.pi/180) * dt),2))
             x+= dx
             y+= dy
-            ang+= dtheta
+            #plt.plot([x, dx], [y, dy], color="blue")
+            #plt.show()
+            #print('adding',x,y)
             
         if ang >= 360 or ang<0:
             ang=ang%360
+            
         #ang=180*ang/3.14
         x=int(round(x/2)*2)
-        y=int(round(x/2)*2)
-        print(x,y,ang)
+        y=int(round(y/2)*2)
+        #print('act',x,y,ang)
         #ang=int((round(ang/15)*15)//15)
         new_node = [int(x),int(y),int(ang)]        
         
@@ -50,12 +59,13 @@ class actionSet:
     def getobstaclespace(self):
         oblist1=[]       #List to store the obstacle coordinates for final animation
         riglist=set([])
-        radius=38
+        radius=17.7
         clearance=5
         dist= radius + clearance
         #xmax=510
         #ymax=510
         #print('ob space')
+        
         for x in range(0,1001):
             for y in range(0,1001):
                 
